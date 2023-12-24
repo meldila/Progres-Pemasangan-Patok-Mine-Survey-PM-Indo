b6cedec89a1b21903f75a149cb4b1561653b8d11
meldila <154612886+meldila@users.noreply.github.com>
2023-12-24 11:40:43 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1QCyEz34lnaVYHtH5FN__IgDwHRZZS1RzHxvgFAfqwyI/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDHB72j5nz-uuKwa0pHatfOkY2xh5HFZHM';
